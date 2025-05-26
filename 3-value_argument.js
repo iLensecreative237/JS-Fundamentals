c98602415eb7b72e7b59e59f7ca0941deb5c9fe9
ilensecreative237 <chuiya2740@gmail.com>
2025-05-26 12:24:36 +0100

    made corrections to task 3  Value of my argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,3 @@
-const args = process.argv.slice(2);
+const arg = process.argv[2];
 
-if (args[0] === undefined) {
-  console.log("No argument");
-} else {
-  console.log(args[0]);
-}
+console.log(arg !== undefined ? arg : "No argument");
