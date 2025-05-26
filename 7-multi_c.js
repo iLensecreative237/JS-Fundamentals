dbcb6f6d395173156bb9a6ff6e21f69e36d5c3b1
ilensecreative237 <chuiya2740@gmail.com>
2025-05-26 10:24:10 +0100

    task 7 I love C

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const x = Number(process.argv[2]);
+
+if (Number.isInteger(x)) {
+  let i = 0;
+  while (i < x) {
+    console.log("C is fun");
+    i++;
+  }
+} else {
+  console.log("Missing number of occurrences");
+}
