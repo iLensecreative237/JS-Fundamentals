5040965b3ce88eae91c6c47e447004b354bbe572
ilensecreative237 <chuiya2740@gmail.com>
2025-05-27 14:24:12 +0100

    made corrections to task 10 on factorial

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,8 +1,9 @@
-function add(a, b) {
-  return a + b;
+function factorial(n) {
+  if (isNaN(n) || n <= 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
-const a = Number(process.argv[2]);
-const b = Number(process.argv[3]);
-
-console.log(add(a, b));
+const input = Number(process.argv[2]);
+console.log(factorial(input));
